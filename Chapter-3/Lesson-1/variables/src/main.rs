c3f90a59686e8f5a85e8f423e75f28063fc14644
Anthony T <anthonyt3@sbcglobal.net>
2024-10-10 17:11:33 -0700

    Chapter 3, Lesson 1 Complete 'Variables, Constants, & Shadowing'

---
+++ Chapter-3/Lesson-1/variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let x = 5;
+    println!("The value of x is: {x}");
+    let x = "six";
+    println!("The value of x is: {x}");
+
+    // const SECONDS_PER_DAY: u32 = 24 * 60 * 60;
+}
