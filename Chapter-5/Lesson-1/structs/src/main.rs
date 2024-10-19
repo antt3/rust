d25e7cdd0ef6ab182bd44e27ddad290f8f667e55
Anthony T <anthonyt3@sbcglobal.net>
2024-10-18 22:35:02 -0700

    Chapter 5, Lesson 1 Complete 'The Struct Basics'

---
+++ Chapter-5/Lesson-1/structs/src/main.rs
@@ -0,0 +1,41 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+struct AlwaysEqual;
+
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("username123"),
+        email: String::from("email@default.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@default.com");
+
+    let user2 = User {
+        email: String::from("random@deault.com"),
+        ..user1
+    };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    let subject = AlwaysEqual;
+}
