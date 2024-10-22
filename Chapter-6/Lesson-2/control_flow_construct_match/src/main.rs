9bb25029be885a9b8b4437d5d346ed5bb009ae1b
Anthony T <anthonyt3@sbcglobal.net>
2024-10-21 22:55:19 -0700

    Chapter 6, Lesson 2 Complete 'Match & _'

---
+++ Chapter-6/Lesson-2/control_flow_construct_match/src/main.rs
@@ -0,0 +1,61 @@
+// US State Enum Value To Be Stored In The Quarter Coin Enum Variant
+#[derive(Debug)]
+enum USState {
+    Alabama,
+    Alaska,
+    // etc.
+}
+
+// Match Arms Using Commas And Curly Brackets
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(USState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
+
+fn main() {
+    value_in_cents(Coin::Quarter(USState::Alaska));
+
+    // Matching With Option<T>
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    // The _ Placeholder
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        // other => move_player(other),
+        // _ => reroll(),
+        _ => (),
+    }
+
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
+    // fn move_player(num_spaces: u8) {}
+    // fn reroll() {}
+}
