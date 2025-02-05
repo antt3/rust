c0c5c8251602b81d502aff3f557f0172ac153d65
Anthony T <anthonyt3@sbcglobal.net>
2025-02-05 00:42:03 -0800

    Chapter 11, Lesson 2 Complete 'Test Specificity'

--- Chapter-11/Lesson-2/adder/src/lib.rs
+++ Chapter-11/Lesson-2/adder/src/lib.rs
@@ -1,3 +1,8 @@
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {a}");
+    10
+}
+
 pub fn add_two(a: usize) -> usize {
     a + 2
 }
@@ -5,6 +10,20 @@ pub fn add_two(a: usize) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    // This code passes & doesn't display the called func's println statement
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(value, 10);
+    }
+
+    // This code fails & displays the called funtion's println statement
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(value, 5);
+    }
 
     #[test]
     fn add_two_and_two() {
