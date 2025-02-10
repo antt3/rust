90f1e6c042f7d6e7b079154a007f015ef8fc5a5b
Anthony T <anthonyt3@sbcglobal.net>
2025-02-10 05:00:42 -0800

    Chapter 12, Lesson 5 Complete 'TDD - Case Sensitive/Insensitive Search'

--- Chapter-12/minigrep/src/main.rs
+++ Chapter-12/minigrep/src/main.rs
@@ -9,9 +9,6 @@ fn main() {
         println!("Problem passing arguments: {err}");
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
     
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
