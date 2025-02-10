76bdaa3c9a5f0e7b085208f071c2096cf4efc28c
Anthony T <anthonyt3@sbcglobal.net>
2025-02-10 05:46:08 -0800

    Chapter 12, Lesson 6 Complete 'stdout/stderr, Env Vars, Added Comments'

--- Chapter-12/minigrep/src/lib.rs
+++ Chapter-12/minigrep/src/lib.rs
@@ -8,14 +8,20 @@ pub struct Config {
 }
 
 impl Config {
+    // Defining a build func because new funcs are not 
+    // expected to have the possibility of erroring
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
 
+        // clone() is not the most efficient method to use here,
+        // it's used for simplicity in the lesson
         let query = args[1].clone();
         let file_path = args[2].clone();
 
+        // If IGNORE_CASE is undefined it will return an error, 
+        // and is_ok() will return false
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
@@ -26,6 +32,8 @@ impl Config {
     }
 }
 
+// A lifetime is required so contents is used
+// since the return value will be a slice of it
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
 
@@ -38,6 +46,8 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+// A lifetime is required so contents is used
+// since the return value will be a slice of it
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
     let mut results = Vec::new();
@@ -51,7 +61,14 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
     results
 }
 
+// Box<dyn Error> is a trait object, more info below:
+// Box<dyn Error> means the func will return a type that implements the Error trait,
+// but I don't have to specify what specific type the return val will be.
+// This gives me flexibility to return dif types in dif error cases.
+// The dyn keyword is short for dynamic.
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // ? will cause the error handling responsibility
+    // to be passed to the function calling run()
     let contents = fs::read_to_string(config.file_path)?;
 
     let results = if config.ignore_case {
@@ -74,6 +91,8 @@ mod tests {
     #[test]
     fn case_sensitive() {
         let query = "duct";
+        // The backslash after the opening double quote tells Rust not to put
+        // a newline character at the beg of the contents of the string literal
         let contents = "\
 Rust:
 safe, fast, productive.
@@ -86,6 +105,8 @@ Duct tape";
     #[test]
     fn case_insensitive() {
         let query = "rUsT";
+        // The backslash after the opening double quote tells Rust not to put
+        // a newline character at the beg of the contents of the string literal
         let contents = "\
 Rust:
 safe, fast, productive.
--- Chapter-12/minigrep/src/main.rs
+++ Chapter-12/minigrep/src/main.rs
@@ -2,16 +2,24 @@ use std::{env, process};
 
 use minigrep::Config;
 
+// Note on println!() vs eprintln!():
+// println outputs to stdout or "standard output"
+// eprintln outputs to stderr or "standard error"
+
+// Exiting with exit code 1 is customary for exiting with an error
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    // Assigns the output or errors out with customized error
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem passing arguments: {err}");
+        eprintln!("Problem passing arguments: {err}");
         process::exit(1);
     });
     
+    // If the output is an error then errors out with custom error
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1)
     }
 }
